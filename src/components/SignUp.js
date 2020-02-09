432428f7e2d566c3a5b43a12b8310842297c9d36
Henry Richardson <hrichardson83@gmail.com>
2020-02-09 12:33:27 -0500

    testing bug

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
-import Title from "./atoms/Title.js";
-import SubmitBtn from "./atoms/SubmitBtn.js";
+import React, { useState, useEffect } from 'react';
+import Title from './atoms/Title.js';
+import SubmitBtn from './atoms/SubmitBtn.js';
 
 function SignUp() {
   const [form, setForm] = useState({
-    email: "",
-    firstName: "",
-    lastName: "",
+    email: '',
+    firstName: '',
+    lastName: '',
     agree: false
   });
   const [content, setContent] = useState({
-    mainTitle: "join the list",
-    subTitle: "sign up for the tlc newsletter.",
-    submitBtn: "next"
+    mainTitle: 'join the list',
+    subTitle: 'sign up for the tlc newsletter.',
+    submitBtn: 'next'
   });
   const [currentStep, setCurrentStep] = useState(1);
 
@@ -20,29 +20,29 @@ function SignUp() {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  function updateContent(contentItem, update) {
+  const updateContent = (contentItem, update) => {
     setContent({ ...content, [contentItem]: update });
-  }
-
-  // useEffect(()=>{
-
-  // })
+    console.log('update content', contentItem, update);
+  };
 
   function handleCurrentStep(step) {
     if (step === 2) {
       setCurrentStep(2);
-      updateContent("subTitle", "Almost Done! Please Enter Your First and Last Name.");
-      updateContent("submitBtn", "sign up");
+      console.log('handle current step', currentStep);
+      updateContent('subTitle', 'Almost Done! Please Enter Your First and Last Name.');
+      updateContent('submitBtn', 'sign up');
     } else if (step === 3) {
       setCurrentStep(3);
-      updateContent("mainTitle", "congratulations!");
-      updateContent("subTitle", "Thank You For Signing Up!");
+      updateContent('mainTitle', 'congratulations!');
+      updateContent('subTitle', 'Thank You For Signing Up!');
     }
   }
+  // useEffect(() => {
+  //   console.log('use effectre-render');
+  // });
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log("next button hit");
     if (currentStep === 1) {
       handleCurrentStep(2);
     } else if (currentStep === 2) {
@@ -66,16 +66,39 @@ function SignUp() {
             <div className="form-group">
               {currentStep === 1 && (
                 <div className="email">
-                  <input type="email" id="email" name="email" placeholder="enter email address" value={form.email} onChange={updateFields} />
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="enter email address"
+                    value={form.email}
+                    onChange={updateFields}
+                  />
                 </div>
               )}
               {currentStep === 2 && (
                 <div className="fullName">
                   <div className="fName">
-                    <input type="text" id="firstName" name="firstName" placeholder="First Name" value={form.firstName} onChange={updateFields} required />
+                    <input
+                      type="text"
+                      id="firstName"
+                      name="firstName"
+                      placeholder="First Name"
+                      value={form.firstName}
+                      onChange={updateFields}
+                      required
+                    />
                   </div>
                   <div className="lName">
-                    <input type="text" id="lastName" name="lastName" placeholder="Last Name" value={form.lastName} onChange={updateFields} required />
+                    <input
+                      type="text"
+                      id="lastName"
+                      name="lastName"
+                      placeholder="Last Name"
+                      value={form.lastName}
+                      onChange={updateFields}
+                      required
+                    />
                   </div>
                 </div>
               )}
@@ -89,11 +112,18 @@ function SignUp() {
             {currentStep === 1 && (
               <div className="gdpr">
                 <div>
-                  <input id="gdpr" name="agree" type="checkbox" checked={form.agree} onChange={updateFields} />
+                  <input
+                    id="gdpr"
+                    name="agree"
+                    type="checkbox"
+                    checked={form.agree}
+                    onChange={updateFields}
+                  />
                 </div>
                 <div>
                   <label htmlFor="gdpr">
-                    <span className="aw">I agree to receive information from</span> <span className="aw">Discovery Communications in accordance</span>{" "}
+                    <span className="aw">I agree to receive information from</span>{' '}
+                    <span className="aw">Discovery Communications in accordance</span>{' '}
                     <span className="aw">
                       with the following <a href="https://www.google.com/">Privacy Policy</a>
                     </span>
